88dd1ac8aa48f8c12d627e96f22c771ce38a7eb0
YesVladess <vlad.kononenko95@gmail.com>
2019-11-15 00:36:38 +0300

    Scroll view image

---
+++ Cassini/DemoURLs.swift
@@ -0,0 +1,15 @@
+//
+//  DemoURLs.swift
+//  Cassini
+//
+//  Created by YesVladess on 14.11.2019.
+//  Copyright © 2019 YesVladess. All rights reserved.
+//
+
+import Foundation
+
+/// URL's containing big images.
+struct DemoURLs {
+    // Stanford's Oval image
+    static let stanford = Bundle.main.url(forResource: "oval", withExtension: "jpg")
+}
